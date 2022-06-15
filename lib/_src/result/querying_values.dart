2e70cdfb559a5ac6c47bbe1784c7687b014a740d
0xba1 <dev.boluwatife@gmail.com>
2022-06-15 22:54:05 +0100

    isOkAnd

--- lib/_src/result/querying_values.dart
+++ lib/_src/result/querying_values.dart
@@ -18,4 +18,25 @@ extension QueryingValues<T, E> on Result<T, E> {
   bool isOk() {
     return _type == ResultType.ok;
   }
+
+  /// Returns `true` if the result is `ok` and the value matches the predicate `f`
+  ///
+  /// # Examples
+  ///
+  /// ```dart
+  /// Result<int, String> x = ok(9);
+  /// expect(x.isOkAnd((int val) => val > 5), true);
+  ///
+  /// expect(x.isOkAnd((int val) => val < 5), false);
+  ///
+  /// Result<int, String> y = err('An unexpected error occured');
+  /// expect(x.isOk(), false);
+  /// ```
+  bool isOkAnd(bool Function(T) f) {
+    if (_type == ResultType.ok) {
+      return f(_ok);
+    }
+
+    return false;
+  }
 }
