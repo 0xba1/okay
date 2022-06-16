d475bedda5caf6de4e0ca7f4344bdad228b00e4f
0xba1 <dev.boluwatife@gmail.com>
2022-06-16 17:22:09 +0100

    err()

--- lib/_src/result/adapter.dart
+++ lib/_src/result/adapter.dart
@@ -23,4 +23,26 @@ extension Adapter<T, E> on Result<T, E> {
         return null;
     }
   }
+
+  /// Converts from `Result<T, E>` to `E?`.
+  ///
+  /// # Examples
+  ///
+  /// Basic usage:
+  ///
+  /// ```dart
+  /// Result<int, String> x = ok(2);
+  /// expect(x.err(), null);
+  ///
+  /// Result<int, String> y = err("An error occured");
+  /// expect(y.err(), "An error occured");
+  /// ```
+  E? err() {
+    switch (_type) {
+      case ResultType.err:
+        return _errValue;
+      case ResultType.ok:
+        return null;
+    }
+  }
 }
