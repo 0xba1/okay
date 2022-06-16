ad46b0e1a92c717347742a3672c76243bf040488
0xba1 <dev.boluwatife@gmail.com>
2022-06-16 17:21:49 +0100

    ok()

--- lib/_src/result/adapter.dart
+++ lib/_src/result/adapter.dart
@@ -1,4 +1,26 @@
 part of '../result.dart';
 
 /// Adapter for each variant
-extension Adapter<T, E> on Result<T, E> {}
+extension Adapter<T, E> on Result<T, E> {
+  /// Converts from `Result<T, E>` to `T?`.
+  ///
+  /// # Examples
+  ///
+  /// Basic usage:
+  ///
+  /// ```dart
+  /// Result<int, String> x = ok(2);
+  /// expect(x.ok(), 2);
+  ///
+  /// Result<int, String> y = err("An error occured");
+  /// expect(y.ok(), null);
+  /// ```
+  T? ok() {
+    switch (_type) {
+      case ResultType.ok:
+        return _okValue;
+      case ResultType.err:
+        return null;
+    }
+  }
+}
