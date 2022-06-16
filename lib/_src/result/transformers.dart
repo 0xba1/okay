26ce0e3f60ba67a9fbd36028243916456aba2368
0xba1 <dev.boluwatife@gmail.com>
2022-06-16 17:24:34 +0100

    Result.mapOr

--- lib/_src/result/transformers.dart
+++ lib/_src/result/transformers.dart
@@ -35,4 +35,25 @@ extension Transformers<T, E> on Result<T, E> {
         return Result.err(_err);
     }
   }
+
+  /// Returns the provided fallback (if `err` (failure)), or
+  /// applies a function to the contained value (if `ok` (success)).
+  ///
+  /// # Examples
+  ///
+  /// ```dart
+  /// Result<String, String> x = ok("foo");
+  /// expect(x.map_or(42, (val) => val.length), 3);
+  ///
+  /// Result<String, String> y = err("foo");
+  /// expect(y.map_or(42, (val) => val.length), 42);
+  /// ```
+  U mapOr<U>(U fallback, U Function(T) op) {
+    switch (_type) {
+      case ResultType.ok:
+        return op(_ok);
+      case ResultType.err:
+        return fallback;
+    }
+  }
 }
