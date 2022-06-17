79ca7badc11ed91566cce950fad1cb4298e7784a
0xba1 <dev.boluwatife@gmail.com>
2022-06-17 13:07:31 +0100

    Result.contains

--- lib/_src/result/contains.dart
+++ lib/_src/result/contains.dart
@@ -1,4 +1,29 @@
 part of '../result.dart';
 
 /// Compairs contained `ok` or `err` value
-extension Contains<T, E> on Result<T, E> {}
+extension Contains<T, E> on Result<T, E> {
+  /// Returns `true` if the result is an `ok` (success) value
+  /// containing the given value.
+  ///
+  /// ## Examples
+  ///
+  /// Basic usage:
+  /// ```dart
+  /// Result<int, String> x = ok(2);
+  /// expect(x.contains(2), true);
+  ///
+  /// Result<int, String> x = ok(3);
+  /// expect(x.contains(2), false);
+  ///
+  /// Result<int, String> x = err('Some error message');
+  /// expect(x.contains(2), false);
+  /// ```
+  bool contains(Object x) {
+    switch (_type) {
+      case ResultType.ok:
+        return _ok == x;
+      case ResultType.err:
+        return false;
+    }
+  }
+}
