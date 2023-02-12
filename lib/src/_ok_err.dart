f8e4f820eae002bf3cc7534680f932be3990b6f4
0xba1 <dev.boluwatife@gmail.com>
2023-02-12 14:31:56 +0100

    Breaking change: Removed ok() and err()

--- lib/src/_ok_err.dart
+++
@@ -1,7 +0,0 @@
-import 'package:okay/src/_result.dart';
-
-/// Success `Result`
-Result<T, E> ok<T, E>(T okValue) => Result.ok(okValue);
-
-/// Failure `Result`
-Result<T, E> err<T, E>(E errValue) => Result.err(errValue);
